af64a95c38205258ee409028ae794cf0ba202f1f
Dennis <dennis@Zettas-MacBook-Pro.local>
2023-08-22 16:18:59 +0300

    Initial

---
+++ python-chatgpt.py
@@ -0,0 +1,14 @@
+# python v.3.9.6
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
+
+# To use only dependencies involved to the project
+# pip install pipreqs
+# pipreqs
+# pipreqs --force
+
+import requests
+
+api_endpoint = "https://api.openai.com/v1/completions"
+
+print('Hello!')
