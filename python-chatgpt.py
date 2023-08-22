33c34841ff5a66f2ed57416a1eaba0d61e2dd1b0
Dennis <dennis@Zettas-MacBook-Pro.local>
2023-08-22 17:14:16 +0300

    Initial

--- python-chatgpt.py
+++ python-chatgpt.py
@@ -10,5 +10,23 @@
 import requests
 
 api_endpoint = "https://api.openai.com/v1/completions"
+api_key = "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"
 
-print('Hello!')
+request_headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + api_key,
+
+}
+
+request_data = {
+    "model": "text-davinci-003",
+    "prompt": "Write python script for hello world",
+    "max_tokens": 100,
+    "temperature": 0.5
+}
+
+response = requests.post(api_endpoint, headers=request_headers, json=request_data)
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Request failed with status code: {response.status_code}")
